a3c2172070753391e9acf8fd19809b8f5be43773
LeoNLe <lnle125@gmail.com>
2018-02-25 14:55:16 -0800

    Breezometer & GoogleMap keys Added

--- app/src/main/java/ApiKeys.java
+++ app/src/main/java/ApiKeys.java
@@ -3,5 +3,7 @@
  */
 
 public class ApiKeys {
-    public static  String weatherAPIKeys = "e3683e271666b131";
+    public final static  String weatherAPIKeys = "e3683e271666b131";
+    public final static String breezoMeterKeys = "b9023667dc3046d1ad00d71dc2772793";
+    public  final static String googleMapKeys = "AIzaSyDy0fndFo8Y2qQQjFS6i9u895eISLxCIs8";
 }
