efba7dd74588a66ab7d9864400e8209eab8485dd
Brennan <linbrennan@yahoo.com>
2018-03-11 17:46:13 -0700

    Fixed errors
    Made new profile creation activity file and moved everything there

---
+++ app/src/main/java/com/example/leole/rectivity/ProfileCreation.java
@@ -0,0 +1,15 @@
+package com.example.leole.rectivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class ProfileCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_creation);
+    }
+}
